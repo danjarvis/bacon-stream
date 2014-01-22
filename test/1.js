eb0d561fa7d02cdd1e19cac96a9ff4840551d8e0
Dan Jarvis <djarvis@kmbs.konicaminolta.us>
2014-01-21 22:38:22 -0500

    test/1.js :: remove 'new'

--- test/1.js
+++ test/1.js
@@ -2,7 +2,7 @@ var test = require('tap').test
   , bacon = require('..');
 
 test('options', function(t) {
-  var b = new bacon({
+  var b = bacon({
       type: 'all-meat'
     , paras: 10
     , sentences: 100
@@ -15,10 +15,10 @@ test('options', function(t) {
   t.equal(b.options.startWithLorem, true, 'startWithLorem set');
   t.equal(b.options.https, true, 'https set');
 
-  b = new bacon('all-meat');
+  b = bacon('all-meat');
   t.equal(b.options.type, 'all-meat', 'type as string set');
 
-  b = new bacon(100);
+  b = bacon(100);
   t.equal(b.options.paras, 100, 'paras as number set');
 
   t.end();
