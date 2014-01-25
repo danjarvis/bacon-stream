567aed0a0d322905986c1197cc895ecb61227530
Dan Jarvis <djarvis@kmbs.konicaminolta.us>
2014-01-25 00:58:48 -0500

    test/1.js :: tests for proper query string generation

--- test/1.js
+++ test/1.js
@@ -1,17 +1,17 @@
 var test = require('tap').test
-  , bacon = require('..');
+  , util = require('util')
+  , query = require('querystring')
+  , bacon = require('../');
 
 test('options', function(t) {
   var b = bacon({
       type: 'all-meat'
     , paras: 10
-    , sentences: 100
     , startWithLorem: true
     , https: true
   });
   t.equal(b.options.type, 'all-meat', 'type set');
   t.equal(b.options.paras, 10, 'paras set');
-  t.equal(b.options.sentences, 100, 'sentences set');
   t.equal(b.options.startWithLorem, true, 'startWithLorem set');
   t.equal(b.options.https, true, 'https set');
   t.equal(b._requests.length, 1, 'one request in queue');
@@ -22,5 +22,38 @@ test('options', function(t) {
   b = bacon(100);
   t.equal(b.options.paras, 100, 'paras as number set');
 
+  t.end();
+});
+
+test('default query', function(t) {
+  var b = bacon()
+    , r = b._requests[0]
+    , s = r.substring(r.indexOf('?') + 1)
+    , q = query.parse(s);
+
+  t.comment(r);
+  t.comment(util.inspect(q));
+
+  t.ok(q, 'has query string');
+  t.equal(Object.keys(q).length, 2, 'has two query parameters');
+  t.equal(q.type, 'meat-and-filler', 'query type set');
+  t.equal(q.paras, '5', 'query paras set');
+  t.end();
+});
+
+test('meaty-sentences query', function(t) {
+  var b = bacon({type: 'all-meat', sentences: 23})
+    , r = b._requests[0]
+    , s = r.substring(r.indexOf('?') + 1)
+    , q = query.parse(s);
+
+  t.comment(r);
+  t.comment(util.inspect(q));
+
+  t.ok(q, 'has query string');
+  t.equal(Object.keys(q).length, 2, 'has two query parameters');
+  t.equal(q.type, 'all-meat', 'query type set');
+  t.equal(q.sentences, '23', 'query sentences set');
+  t.type(q.paras, 'undefined', 'query paras NOT set');
   t.end();
 });
